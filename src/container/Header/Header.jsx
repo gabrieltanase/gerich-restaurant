02f97a6a250a52e8d86d5b29d2d498341b6303fc
Gabriel Tanase <gabycj88@gmail.com>
2022-03-13 16:02:51 +0100

    Gericht Website

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+
+import { images } from '../../constants';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title='Chase the new flavor' />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+        <p className='p__opensans' style={{ margin: '2rem 0'}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt praesentium eos consequatur autem eveniet officiis labore harum quibusdam reiciendis rerum?
+        </p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt='header img' />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
